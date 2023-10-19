98947073e06b061ba5e236e4684d0178a8327767
jah74 <jarrieta74@hotmai.com>
2023-10-19 20:58:23 -0300

    desafio-filipao codificadoo por josé no dia 19/10/23

---
+++ desafio-aluno-jose.js
@@ -0,0 +1,29 @@
+
+let nomeHeroi = "kipper"
+let XP = 3.788
+
+if(XP <1.000){
+console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Ferro")   
+}
+else if(XP >1.001 && XP <2.000){
+console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Bronze")   
+}
+if(XP >2.001 && XP <6.000){
+console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Prata")   
+}
+if(XP >6.001 && XP <7.000){
+console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Ouro")   
+}
+if(XP >7.001 && XP <.8000){
+console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Platina")   
+}
+if(XP >8.001 && XP <9.000){
+console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Ascendent")   
+}
+if(XP >9.001 && XP <10.000){
+console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Imortal")   
+}
+if(XP >=10.000){
+console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Radiante")   
+}
+
