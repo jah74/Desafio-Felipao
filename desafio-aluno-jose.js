26556163f3b6b357dd8dac7ce91ac11d178e7574
jah74 <jarrieta74@hotmai.com>
2023-10-19 21:28:46 -0300

    desfio codificado por José no dia 19/10/23

--- desafio-aluno-jose.js
+++ desafio-aluno-jose.js
@@ -1,29 +1,29 @@
 
 let nomeHeroi = "kipper"
-let XP = 3.788
+let XP = 4500
 
-if(XP <1.000){
+if(XP <1000){
 console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Ferro")   
 }
-else if(XP >1.001 && XP <2.000){
+else if(XP >1001 && XP <2000){
 console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Bronze")   
 }
-if(XP >2.001 && XP <6.000){
+if(XP >2001 && XP <6000){
 console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Prata")   
 }
-if(XP >6.001 && XP <7.000){
+if(XP >6001 && XP <7000){
 console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Ouro")   
 }
-if(XP >7.001 && XP <.8000){
+if(XP >7001 && XP <8000){
 console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Platina")   
 }
-if(XP >8.001 && XP <9.000){
+if(XP >8001 && XP <9000){
 console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Ascendent")   
 }
-if(XP >9.001 && XP <10.000){
+if(XP >9001 && XP <10000){
 console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Imortal")   
 }
-if(XP >=10.000){
+if(XP >=10000){
 console.log("O heroi de nome:",nomeHeroi, "esta no nivel de: Radiante")   
 }
 
